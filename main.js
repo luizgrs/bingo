dd114d6b30c8cbd04ee3fb4aa3409ea9c15dead3
luizgrs <luizgrs@gmail.com>
2020-06-24 20:12:35 -0300

    cantando numeros!

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var parametros;
+var numeros = [];
 
 function start(){
     window.addEventListener('load', windowLoaded);
@@ -24,6 +25,11 @@ function windowLoaded(){
 
     $('btnCantarNumeros').addEventListener('click', function(){
         mudarSecao('cantar', true);
+        iniciaCantar(true);
+    });
+
+    $('btnNovoNumero').addEventListener('click', function(){
+        sortearNumero();
     });
 
     $('novaCartela').addEventListener('click', function(){
@@ -37,11 +43,57 @@ function windowLoaded(){
     atualizaSecao();
 }
 
+function iniciaCantar(zerar){
+    var cantados = [];
+
+    if(!zerar && parametros.cantados){
+        JSON.parse(atob(parametros.cantados)).forEach(function(numero){
+            var novoSorteado = document.createElement('li');
+            novoSorteado.innerText = numero;
+            $('sorteados').appendChild(novoSorteado);        
+        });
+    }
+
+    numeros = shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+    31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
+    46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
+    61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]); 
+
+    if(zerar || !parametros.cantados){
+        parametros.cantados = btoa(JSON.stringify([]));
+        sortearNumero();
+    }
+
+    
+}
+
+function sortearNumero(){
+    var cantados = JSON.parse(atob(parametros.cantados)),
+        numero;
+    do {
+        numero = numeros.pop()
+    }while(cantados.indexOf(numero) !== -1 || !numeros.length)
+    
+    cantados.push(numero);
+
+    $('sorteado').innerText = numero;
+
+    var novoSorteado = document.createElement('li');
+    novoSorteado.innerText = numero;
+    $('sorteados').appendChild(novoSorteado);
+
+    parametros.cantados = btoa(JSON.stringify(cantados));
+
+    updateUrl(parametros, true);
+}
+
 function atualizaSecao(){
     var secao = parametros.mode;
 
     switch(parametros.mode){
         case 'cantar':
+            iniciaCantar();
             break;
         case 'cartela':
             iniciaCartela();
