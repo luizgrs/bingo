1d91835ac31b0e0e67e3ec8a3fcbe417de7ab300
luizgrs <luizgrs@gmail.com>
2020-08-08 23:35:37 -0300

    conserta sorteio do último número e refresh depois de todos numeros sorteados

--- main.js
+++ main.js
@@ -54,19 +54,24 @@ function iniciaCantar(zerar){
         li.classList.remove('marcado');
     });
 
+    numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+        16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+        31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
+        46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
+        61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]; 
+
     if(!zerar && parametros.cantados){
         var cantados = JSON.parse(atob(parametros.cantados));
-        cantados.forEach(marcarSorteado);
+        cantados.forEach(function(numero){
+            marcarSorteado(numero);
+            numeros.splice(numeros.indexOf(numero), 1);
+        });
 
         if(cantados.length)
             atualizaNumeroSorteado(cantados[cantados.length-1]);
     }
 
-    numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-    16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
-    31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
-    46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
-    61,62,63,64,65,66,67,68,69,70,71,72,73,74,75]; 
+
 
     if(zerar || !parametros.cantados){
         parametros.cantados = btoa(JSON.stringify([]));
@@ -86,12 +91,16 @@ function marcarSorteado(numero){
 }
 
 function sortearNumero(){
+    
+    if(!numeros.length)
+        return;
+
     var cantados = JSON.parse(atob(parametros.cantados)),
         numero;
     do {
         numeros = shuffle(numeros);
         numero = numeros.pop()
-    }while(cantados.indexOf(numero) !== -1 || !numeros.length)
+    }while(cantados.indexOf(numero) !== -1 && numeros.length)
     
     cantados.push(numero);
 
